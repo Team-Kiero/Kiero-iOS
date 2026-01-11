019fd232ff5317c468201af2099bdfb5c5b5ac08
KooWihC <ahnchiwook@hanmail.net>
2026-01-12 01:56:49 +0900

    [feat] Config 추가

--- Kiero/Kiero/Network/Base/Config.swift
+++ Kiero/Kiero/Network/Base/Config.swift
@@ -11,6 +11,7 @@ enum Config {
     enum Keys {
         enum Plist {
             static let BASE_URL = "BASE_URL"
+            static let KAKAO_NATIVE_APP_KEY = "KAKAO_NATIVE_APP_KEY"
         }
     }
     
@@ -28,5 +29,19 @@ extension Config {
             fatalError("BASE_URL is not set in plist for this configuration")
         }
         return "http://" + key
+    }()
+    
+    static let apiBaseURL: URL = {
+        guard let url = URL(string: baseURL) else {
+            fatalError("구성된 BASE_URL이 유효한 URL 형식이 아닙니다: \(baseURL)")
+        }
+        return url
+    }()
+    
+    static let kakaoAppKey: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.KAKAO_NATIVE_APP_KEY] as? String else {
+            fatalError("KAKAO_NATIVE_APP_KEY is not set in plist for this configuration")
+        }
+        return key
     }()
 }
