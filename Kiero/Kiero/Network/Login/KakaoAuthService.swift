9a8db145f6af1c743d2264ae5269b28ce787c5cd
KooWihC <ahnchiwook@hanmail.net>
2026-01-12 02:09:07 +0900

    [feat] KakaoAuthService 구현

---
+++ Kiero/Kiero/Network/Login/KakaoAuthService.swift
@@ -0,0 +1,28 @@
+//
+//  KakaoAuthService.swift
+//  Kiero
+//
+//  Created by 안치욱 on 1/9/26.
+//
+
+
+import Combine
+import KakaoSDKUser
+
+final class KakaoAuthService {
+    func loginWithKakao() -> AnyPublisher<String, Error> {
+        Future { promise in
+            if UserApi.isKakaoTalkLoginAvailable() {
+                UserApi.shared.loginWithKakaoTalk { token, error in
+                    if let error = error { promise(.failure(error)) }
+                    else { promise(.success(token?.accessToken ?? "")) }
+                }
+            } else {
+                UserApi.shared.loginWithKakaoAccount { token, error in
+                    if let error = error { promise(.failure(error)) }
+                    else { promise(.success(token?.accessToken ?? "")) }
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+}
