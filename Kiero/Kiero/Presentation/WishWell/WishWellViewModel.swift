b42370b196565d62618be02ce4fa772446a07f80
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-01-13 18:34:36 +0900

    [chore] 목데이터 내용 변경

--- Kiero/Kiero/Presentation/WishWell/WishWellViewModel.swift
+++ Kiero/Kiero/Presentation/WishWell/WishWellViewModel.swift
@@ -17,7 +17,7 @@ struct Wish: Codable {
 final class WishWellViewModel: BaseViewModel {
 
     private(set) var wishList: [Wish] = []
-    let userName: String = "근영"
+    let userName: String = "윤아"
     var currentCoinCount: Int = 350
     
     func fetchWishList(completion: @escaping () -> Void) {
@@ -26,11 +26,11 @@ final class WishWellViewModel: BaseViewModel {
             Wish(name: "용돈 5,000원 받기", price: 50),
             Wish(name: "야구장 직관가기", price: 100),
             Wish(name: "저녁으로 치킨 먹기", price: 100),
-            Wish(name: "테스트 소원 1", price: 10),
-            Wish(name: "테스트 소원 2", price: 20),
-            Wish(name: "테스트 소원 3", price: 30),
-            Wish(name: "테스트 소원 4", price: 40),
+            Wish(name: "데이식스 팬싸인회 가기", price: 100),
             Wish(name: "데이식스 콘서트 가기", price: 350),
+            Wish(name: "박성찐이야 보기", price: 20),
+            Wish(name: "행복 앱잼하기", price: 30),
+            Wish(name: "아요랑 맛있는거 먹기", price: 40),
             Wish(name: "집에 가고 싶다", price: 40)
         ]
         completion()
