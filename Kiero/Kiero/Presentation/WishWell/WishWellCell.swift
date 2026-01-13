b4eb60427b0eadb47703c1f62e1df3592b9d2714
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-01-13 21:30:52 +0900

    [chore] 코드 정렬

--- Kiero/Kiero/Presentation/WishWell/WishWellCell.swift
+++ Kiero/Kiero/Presentation/WishWell/WishWellCell.swift
@@ -32,8 +32,8 @@ final class WishWellCell: UICollectionViewCell {
     }
     
     required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Setup Methods
     
