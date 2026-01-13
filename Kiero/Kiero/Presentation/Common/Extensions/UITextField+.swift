37f65000070d12ff3e5b79e9259adcd108bd15bb
KooWihC <ahnchiwook@hanmail.net>
2026-01-13 20:21:51 +0900

    [feat] Textfield 양 옆 패딩추가 extension 구현

---
+++ Kiero/Kiero/Presentation/Common/Extensions/UITextField+.swift
@@ -0,0 +1,28 @@
+//
+//  UITextField+.swift
+//  Kiero
+//
+//  Created by 안치욱 on 1/13/26.
+//
+
+
+import UIKit
+
+extension UITextField {
+    func addLeftPadding(_ width: CGFloat = 10) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+    
+    func addRightPadding(_ width: CGFloat = 10) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.rightView = paddingView
+        self.rightViewMode = ViewMode.always
+    }
+    
+    func addPadding(leftAmount: CGFloat = 10, rightAmount: CGFloat = 10) {
+        addLeftPadding(leftAmount)
+        addRightPadding(rightAmount)
+    }
+}
