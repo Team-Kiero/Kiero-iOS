459475558dfec778fa03486f533685c5f5e30cde
KooWihC <ahnchiwook@hanmail.net>
2026-01-14 20:56:34 +0900

    [feat] ProfileBox 구현

---
+++ Kiero/Kiero/Presentation/Common/Components/ProfileBox.swift
@@ -0,0 +1,93 @@
+//
+//  ProfileBox.swift
+//  Kiero
+//
+//  Created by 안치욱 on 1/14/26.
+//
+
+import UIKit
+
+import Kingfisher
+import SnapKit
+import Then
+
+final class ProfileBox: UIView {
+    
+    // MARK: - Properties
+    
+    var onTap: (() -> Void)?
+    
+    // MARK: - UI Components
+    
+    private let profileImageView = UIImageView().then {
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 15
+        $0.contentMode = .scaleAspectFill
+        $0.isUserInteractionEnabled = true
+    }
+    
+    private let nameLabel = UILabel().then {
+        $0.textColor = .white
+        $0.isUserInteractionEnabled = true
+    }
+    
+    // MARK: - Life Cycle
+    
+    init(name: String, profileURL: String) {
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
+        configure(name: name, url: profileURL)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting Methods
+    
+    private func setUI() {
+        addSubviews(
+            nameLabel,
+            profileImageView
+        )
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.width.equalTo(100)
+            $0.height.equalTo(40)
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalTo(profileImageView.snp.leading)
+        }
+        
+        profileImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.
+        }
+    }
+    
+    private func setAction() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(viewDidTap))
+        addGestureRecognizer(tap)
+    }
+    
+    private func configure(name: String, url: String) {
+        nameLabel.setTypo(.body2_16_R, text: name)
+        profileImageView.kf.setImage(with: URL(string: url), placeholder: UIImage.icParentProfile)
+    }
+    
+    @objc
+    private func viewDidTap() {
+        onTap?()
+    }
+}
+
+
+#Preview {
+    ProfileBox(name: "안치욱", profileURL: "https://img1.kakaocdn.net/thumb/R640x640.q70/?fname=https://t1.kakaocdn.net/account_images/default_profile.jpeg")
+}
