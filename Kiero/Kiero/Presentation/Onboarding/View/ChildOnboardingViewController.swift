041d42ebc36db8c561bd202e461b5f599efd0f07
KooWihC <ahnchiwook@hanmail.net>
2026-01-17 01:28:04 +0900

    [feat] 자녀 스토리 뷰 구현

---
+++ Kiero/Kiero/Presentation/Onboarding/View/ChildOnboardingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChildOnboardingViewController.swift
+//  Kiero
+//
+//  Created by 안치욱 on 1/16/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ChildOnboardingViewController: BaseViewController<BaseViewModel> {
+    
+    // MARK: - UI Components
+    
+    private let storyImageView = UIImageView()
+    
+    private let speechField = SpeechField()
+    
+    override func setUI() {
+        
+    }
+    
+    override func setLayout() {
+        
+    }
+    
+}
