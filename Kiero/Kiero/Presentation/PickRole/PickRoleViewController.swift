bfe519a515242fee3884063515c5f0a53177bee5
KooWihC <ahnchiwook@hanmail.net>
2026-01-17 01:16:14 +0900

    [feat] 역할 선택 뷰 구현

---
+++ Kiero/Kiero/Presentation/PickRole/PickRoleViewController.swift
@@ -0,0 +1,69 @@
+//
+//  PickRoleViewController.swift
+//  Kiero
+//
+//  Created by 안치욱 on 1/15/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PickRoleViewController: BaseViewController<BaseViewModel> {
+    
+    //private let viewModel: PickRoleViewModelType
+    
+    private let logoImageView = UIImageView().then {
+        $0.image = .imgLogo
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let descriptionLabel = UILabel().then {
+        $0.textColor = .schedule1
+        $0.textAlignment = .center
+    }
+    
+    private let parentButton = RolePickButton(type: .parent)
+    
+    private let childButton = RolePickButton(type: .child)
+    
+    override func setStyle() {
+        descriptionLabel.setTypo(.body1_18_R, text: "아이의 하루가 모험이 되는 곳")
+    }
+    
+    override func setUI() {
+        view.addSubviews(
+            logoImageView,
+            descriptionLabel,
+            parentButton,
+            childButton
+        )
+    }
+    
+    override func setLayout() {
+        logoImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(186)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(logoImageView.snp.bottom).offset(10)
+        }
+        
+        childButton.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-78)
+        }
+        
+        parentButton.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.bottom.equalTo(childButton.snp.top).offset(-25)
+        }
+    }
+}
+
+#Preview {
+    PickRoleViewController(viewModel: PickRoleViewModel(), diContainer: AppDIContainer.shared)
+}
