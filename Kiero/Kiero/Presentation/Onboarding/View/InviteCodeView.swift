d0d975826195ccc1fd2041ee364b6962ffa530c6
KooWihC <ahnchiwook@hanmail.net>
2026-01-17 01:23:18 +0900

    [feat] 자녀 초대 뷰 구현

---
+++ Kiero/Kiero/Presentation/Onboarding/View/InviteCodeView.swift
@@ -0,0 +1,149 @@
+//
+//  InviteCodeView.swift
+//  Kiero
+//
+//  Created by 안치욱 on 1/16/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class InviteCodeView: UIView {
+    
+    // MARK: - Properties
+    
+    var copyButtonTapped: (() -> Void)?
+    
+    // MARK: - UI Components
+    
+    private let inviteView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 10
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.textColor = .gray800
+    }
+    
+    private let codeLabel = UILabel().then {
+        $0.textColor = .kBlack
+    }
+    
+    private let copyButton = UIButton().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.titleLabel?.adjustsFontForContentSizeCategory = true
+        $0.setImage(.icCopy, for: .normal)
+        $0.tintColor = .white
+        $0.setTitle("복사하기", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.semanticContentAttribute = .forceLeftToRight
+        $0.contentVerticalAlignment = .center
+        $0.contentHorizontalAlignment = .center
+        $0.backgroundColor = .kBlack
+        $0.layer.cornerRadius = 10
+    }
+    
+    private let timeLimitLabel = UILabel().then {
+        $0.textColor = .schedule1
+    }
+    
+    private let noticeLabel = UILabel().then {
+        $0.textColor = .gray200
+        $0.numberOfLines = 0
+    }
+    
+    // MARK: - Life Cycle
+    
+    init() {
+        super.init(frame: .zero)
+        
+        setStyle()
+        setUI()
+        setLayout()
+        addTarget()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting Methods
+    
+    private func setStyle() {
+        backgroundColor = .gray900
+        titleLabel.setTypo(.body4_12_R, text: "우리 아이 초대 코드")
+        codeLabel.setTypo(.head1_22_B, text: "별빛기사단918")
+        inviteView.backgroundColor = .white
+        timeLimitLabel.setTypo(.body4_12_R, text: "유효기간 09:23")
+        noticeLabel.setTypo(.body4_12_R, text: "이 코드를 아이에게 알려주시고,\n회원가입 시 입력하도록 안내해주세요.")
+        noticeLabel.textAlignment = .center
+    }
+    
+    private func setUI() {
+        layer.cornerRadius = 15
+        
+        addSubviews(
+            inviteView,
+            copyButton,
+            timeLimitLabel,
+            noticeLabel
+        )
+        
+        inviteView.addSubviews(
+            titleLabel,
+            codeLabel
+        )
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.width.equalTo(343)
+            $0.height.equalTo(266)
+        }
+        
+        inviteView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(27)
+            $0.centerX.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(49.5)
+            $0.height.equalTo(62)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.centerX.equalToSuperview()
+        }
+        
+        codeLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+            $0.centerX.equalToSuperview()
+        }
+        
+        copyButton.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(49.5)
+            $0.top.equalTo(inviteView.snp.bottom).offset(11)
+            $0.height.equalTo(45)
+        }
+        
+        timeLimitLabel.snp.makeConstraints {
+            $0.top.equalTo(copyButton.snp.bottom).offset(19)
+            $0.centerX.equalToSuperview()
+        }
+        
+        noticeLabel.snp.makeConstraints {
+            $0.top.equalTo(timeLimitLabel.snp.bottom).offset(9)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    private func addTarget() {
+        copyButton.addTarget(self, action: #selector(copyButtonDidTap), for: .touchUpInside)
+    }
+    
+    @objc
+    private func copyButtonDidTap() {
+        UIPasteboard.general.string = codeLabel.text
+        Toast.show(message: "초대코드가 복사되었습니다.")
+    }
+}
---
+++ Kiero/Kiero/Presentation/Onboarding/View/ParentsInviteViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ParentsInviteViewController.swift
+//  Kiero
+//
+//  Created by 안치욱 on 1/16/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ParentsInviteViewController: BaseViewController<BaseViewModel> {
+    
+    // MARK: - UI Components
+    
+    private let profileBox = ProfileBox(name: "스꾸삐", profileURL: "https://k.kakaocdn.net/dn/Aw6ap/dJMcafehtYl/o4UxbOofgNw3FQtDW5VUOk/img_640x640.jpg")
+    
+    private let titleLabel = UILabel().then {
+        $0.textColor = .white
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    private let textField = TextField(type: .parent(.totalName))
+    
+    private let inviteView = InviteCodeView()
+    
+    private let startButton = UIButton().then {
+        $0.backgroundColor = .gray900
+        $0.layer.cornerRadius = 8
+        $0.setTitle("시작하기", for: .normal)
+    }
+    
+    override func setStyle() {
+        titleLabel.setTypo(.title2_20_SB, text: "아직 연결된 자녀 계정이 없어요\n자녀를 추가해주세요!")
+        textField.setText(text: "꾸비")
+    }
+    
+    override func setUI() {
+        view.addSubviews(
+            profileBox,
+            titleLabel,
+            textField,
+            inviteView,
+            startButton
+        )
+    }
+    
+    override func setLayout() {
+        profileBox.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(15)
+            $0.trailing.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(profileBox.snp.bottom).offset(18)
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        textField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(14)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(95)
+        }
+        
+        inviteView.snp.makeConstraints {
+            $0.top.equalTo(textField.snp.bottom)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(343)
+        }
+        
+        startButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-41)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(49)
+        }
+    }
+}
+
+#Preview {
+    ParentsInviteViewController(viewModel: BaseViewModel(), diContainer: AppDIContainer.shared)
+}
