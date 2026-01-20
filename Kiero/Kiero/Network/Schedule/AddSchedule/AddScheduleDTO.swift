28fd116dd11767538ac7ad9b9ec6e82b31665540
hyeyeonie <shy020916@naver.com>
2026-01-21 00:31:56 +0900

    [chore] 폴더 생성 및 파일 분리

---
+++ Kiero/Kiero/Network/Schedule/AddSchedule/AddScheduleDTO.swift
@@ -0,0 +1,8 @@
+//
+//  AddScheduleDTO.swift
+//  Kiero
+//
+//  Created by 신혜연 on 1/21/26.
+//
+
+import Foundation
---
+++ Kiero/Kiero/Network/Schedule/AddSchedule/AddScheduleService.swift
@@ -0,0 +1,9 @@
+//
+//  AddScheduleService.swift
+//  Kiero
+//
+//  Created by 신혜연 on 1/21/26.
+//
+
+import Foundation
+import Combine
