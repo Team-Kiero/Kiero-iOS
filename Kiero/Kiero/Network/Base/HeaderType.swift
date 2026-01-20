d273fed415e256d5e022f8ae7f4d6967cb23d043
KooWihC <ahnchiwook@hanmail.net>
2026-01-21 04:41:43 +0900

    [feat] HeaderType 추가

--- Kiero/Kiero/Network/Base/HeaderType.swift
+++ Kiero/Kiero/Network/Base/HeaderType.swift
@@ -10,16 +10,25 @@ import Foundation
 enum HeaderType {
     case none
     case auth
-    
+    case refreshCookie
+
     var type: [String: String] {
         switch self {
         case .none:
             return ["Content-Type": "application/json"]
+
         case .auth:
             let token = TokenManager.shared.getAccessToken() ?? ""
             return [
                 "Content-Type": "application/json",
                 "Authorization": "Bearer \(token)"
+            ]
+
+        case .refreshCookie:
+            let refresh = TokenManager.shared.getRefreshToken() ?? ""
+            return [
+                "Content-Type": "application/json",
+                "Cookie": "refreshToken=\(refresh)"
             ]
         }
     }
