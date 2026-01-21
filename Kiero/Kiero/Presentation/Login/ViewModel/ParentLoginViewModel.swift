817a3d7e2457edd969fe04155d94d7802afea99b
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-01-22 06:00:51 +0900

    [feat] TokenManager로 프로필과 이름 저장

--- Kiero/Kiero/Presentation/Login/ViewModel/ParentLoginViewModel.swift
+++ Kiero/Kiero/Presentation/Login/ViewModel/ParentLoginViewModel.swift
@@ -58,6 +58,8 @@ final class ParentLoginViewModel: BaseViewModel, ViewModelType {
                 TokenManager.shared.saveAccessToken(loginData.accessToken)
                 TokenManager.shared.saveRefreshToken(loginData.refreshToken)
                 TokenManager.shared.saveUserRole(loginData.role)
+                TokenManager.shared.saveUserName(loginData.name)
+                TokenManager.shared.saveProfile(loginData.image)
 
                 await MainActor.run {
                     self.stateSubject.send(.idle)
