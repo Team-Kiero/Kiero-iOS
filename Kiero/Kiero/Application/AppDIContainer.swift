8383c42abcfcb28f3b901e44107e628c4cf98d0b
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-01-22 04:06:34 +0900

    [feat] 컨테이너 수정

--- Kiero/Kiero/Application/AppDIContainer.swift
+++ Kiero/Kiero/Application/AppDIContainer.swift
@@ -11,6 +11,7 @@ import KakaoSDKAuth
 import Moya
 
 final class AppDIContainer: ViewControllerFactory, ServiceFactory{
+    
     static let shared = AppDIContainer()
     private init() {}
 }
@@ -32,10 +33,10 @@ extension AppDIContainer {
         return PickRoleViewController(viewModel: viewModel, diContainer: self)
     }
     
-//    func makeParentOnboardingViewController() -> UIViewController {
-//        let vm = ParentOnboardingViewModel(name: <#String#>, profileURL: <#String#>)
-//        return ParentOnboardingViewController(viewModel: vm, diContainer: self)
-//    }
+    //    func makeParentOnboardingViewController() -> UIViewController {
+    //        let vm = ParentOnboardingViewModel(name: <#String#>, profileURL: <#String#>)
+    //        return ParentOnboardingViewController(viewModel: vm, diContainer: self)
+    //    }
     
     func makeChildOnboardingViewController() -> UIViewController {
         let items: [SpeechItem] = [
@@ -78,8 +79,19 @@ extension AppDIContainer {
 // MARK: - Notification
 
 extension AppDIContainer {
+    func makeNotificationFeedService() -> FeedServiceType {
+        FeedService()
+    }
+    
+    func makeNotificationFeedViewModel() -> NotificationFeedViewModel {
+        return NotificationFeedViewModel(
+            feedService: makeNotificationFeedService(),
+            scheduleService: makeScheduleService()
+        )
+    }
+    
     func makeNotificationFeedViewController() -> UIViewController {
-        let viewModel = NotificationFeedViewModel()
+        let viewModel = makeNotificationFeedViewModel()
         return NotificationFeedViewController(viewModel: viewModel, diContainer: self)
     }
 }
