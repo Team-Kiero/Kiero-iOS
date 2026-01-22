97155c26127b43c01040be5a291d130c2da4eb3f
hyunseo-han <ols3040015@naver.com>
2026-01-22 16:23:46 +0900

    [feat] 불 피우기 DTO

--- Kiero/Kiero/Network/GiveFireStone/GiveFireStoneDTO.swift
+++ Kiero/Kiero/Network/GiveFireStone/GiveFireStoneDTO.swift
@@ -7,13 +7,7 @@
 
 import Foundation
 
-//struct GiveFireStoneDTO: Decodable {
-//    let status: Int
-//    let message: String
-//    let data: FireLitData?
-//}
-
 struct FireLitData: Decodable {
-    let gotStones: [String] // 용인지
+    let gotStones: [String] 
     let earnedCoinAmount: Int
 }
