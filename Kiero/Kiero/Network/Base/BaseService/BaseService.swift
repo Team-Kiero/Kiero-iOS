85a0e8a215b1de65d4f1cea6e0dfb0a90c5b9ada
KooWihC <ahnchiwook@hanmail.net>
2026-02-26 00:52:01 +0900

    [refactor] BaseService Refactor

--- Kiero/Kiero/Network/Base/BaseService/BaseService.swift
+++ Kiero/Kiero/Network/Base/BaseService/BaseService.swift
@@ -9,75 +9,58 @@ import Foundation
 
 struct AnyEncodable: Encodable {
     private let encodeFunc: (Encoder) throws -> Void
-    init(_ value: Encodable) {
-        self.encodeFunc = value.encode
-    }
-    func encode(to encoder: Encoder) throws {
-        try encodeFunc(encoder)
-    }
+    init(_ value: Encodable) { self.encodeFunc = value.encode }
+    func encode(to encoder: Encoder) throws { try encodeFunc(encoder) }
 }
 
-final class BaseService {
-    
-    static let shared = BaseService()
-    private init() {}
-    
+final class BaseService: NetworkServicing {
+
+    private let tokenRefresher: TokenRefreshing
+
+    init(tokenRefresher: TokenRefreshing) {
+        self.tokenRefresher = tokenRefresher
+    }
+
     func request<Response: Decodable>(
         endPoint: EndPoint,
-        body: Encodable? = nil,
-        didRetry: Bool = false
+        body: Encodable?,
+        didRetry: Bool
     ) async throws -> Response {
-        
+
         do {
             return try await perform(endPoint: endPoint, body: body)
-            
+
         } catch let error as NetworkError {
-            
+
             guard case .clientError(let statusCode) = error,
                   statusCode == 401,
                   didRetry == false else {
                 throw error
             }
-            
+
             switch endPoint.refreshPolicy {
-                
             case .none:
                 throw error
-                
+
             case .child:
-                try await TokenRefresher.shared.refreshAllTokens()
-                
+                try await tokenRefresher.refreshAllTokens()
+
             case .parent:
-                do {
-                    try await TokenRefresher.shared.refreshAccessToken()
-                } catch {
-                    try await TokenRefresher.shared.refreshAllTokens()
-                }
+                do { try await tokenRefresher.refreshAccessToken() }
+                catch { try await tokenRefresher.refreshAllTokens() }
             }
-            
-            return try await request(
-                endPoint: endPoint,
-                body: body,
-                didRetry: true
-            )
+
+            return try await request(endPoint: endPoint, body: body, didRetry: true)
         }
     }
-    
+
     private func perform<Response: Decodable>(
         endPoint: EndPoint,
         body: Encodable?
     ) async throws -> Response {
-        
-        let request = try RequestBuilder.build(
-            endPoint: endPoint,
-            body: body
-        )
-        
+
+        let request = try RequestBuilder.build(endPoint: endPoint, body: body)
         let (data, httpResponse) = try await NetworkExecutor.execute(request)
-        
-        return try ResponseDecoder.decode(
-            data: data,
-            statusCode: httpResponse.statusCode
-        )
+        return try ResponseDecoder.decode(data: data, statusCode: httpResponse.statusCode)
     }
 }
