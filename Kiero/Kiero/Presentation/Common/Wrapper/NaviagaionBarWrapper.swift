300899d1d863395ce3f97efb0434ad0915a4d3aa
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-03-02 23:30:48 +0900

    [chore] wrapper 폴더 추가

---
+++ Kiero/Kiero/Presentation/Common/Wrapper/NaviagaionBarWrapper.swift
@@ -0,0 +1,45 @@
+//
+//  NaviagaionBarWrapper.swift
+//  Kiero
+//
+//  Created by 정윤아 on 3/2/26.
+//
+
+import SwiftUI
+import UIKit
+
+struct NavigationBarWrapper: UIViewRepresentable {
+    
+    // MARK: - Properties
+    
+    let type: NavigationBarType
+    var title: String? = nil
+    var isRightButtonEnabled: Bool = true
+    var isNotificationActive: Bool = false
+    
+    var onLeftTap: (() -> Void)?
+    var onRightTap: (() -> Void)?
+
+    func makeUIView(context: Context) -> NavigationBar {
+        let view = NavigationBar(type: type)
+        
+        view.leftButtonAction = {
+            onLeftTap?()
+        }
+        
+        view.rightButtonAction = {
+            onRightTap?()
+        }
+        
+        return view
+    }
+
+    func updateUIView(_ uiView: NavigationBar, context: Context) {
+        uiView.isRightButtonEnabled = isRightButtonEnabled
+        uiView.isNotificationActive = isNotificationActive
+        
+        if let title = title {
+            uiView.updateTitle(title)
+        }
+    }
+}
