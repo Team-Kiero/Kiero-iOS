c66d41e6d935fc61f40ed1cbfe2987e93f5a61bb
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-03-02 16:50:25 +0900

    [feat] 마이페이지 VM 구현

---
+++ Kiero/Kiero/Presentation/MyPage/MyPageViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageViewModel.swift
+//  Kiero
+//
+//  Created by 정윤아 on 3/2/26.
+//
+
+import Combine
+import SwiftUI
+
+final class MyPageViewModel: ObservableObject {
+    
+    @Published var userName: String = "꾸비"
+    @Published var userImage: String? = nil
+    @Published var connectedChild: Int = 0
+    
+}
