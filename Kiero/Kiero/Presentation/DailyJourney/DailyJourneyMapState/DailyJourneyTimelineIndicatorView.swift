ddf341c2232eebadf9d6910648b4077d6d96922d
hyunseo-han <ols3040015@naver.com>
2026-03-02 23:41:17 +0900

    [fix] 아이콘 컬러 로직 수정

--- Kiero/Kiero/Presentation/DailyJourney/DailyJourneyMapState/DailyJourneyTimelineIndicatorView.swift
+++ Kiero/Kiero/Presentation/DailyJourney/DailyJourneyMapState/DailyJourneyTimelineIndicatorView.swift
@@ -7,30 +7,44 @@
 
 import SwiftUI
 
-struct DailyJourneyTimelineIndicatorView: View{
+struct DailyJourneyTimelineIndicatorView: View {
     let isOngoing: Bool
+    let status: String
+    
+    private var shouldShowRectangle: Bool {
+        isOngoing || status == "PENDING"
+    }
     
     private var iconColor: Color {
-        isOngoing ? .main : .white
+        if isOngoing {
+            return .main
+        } else if status == "PENDING" {
+            return .white
+        } else {
+            return .white.opacity(0.5)
+        }
+    }
+    
+    private var hasGlow: Bool {
+        isOngoing
     }
     
     var body: some View {
-        VStack (spacing: -2) {
-            if isOngoing {
+        VStack(spacing: 0) {
+            if shouldShowRectangle {
                 Image("ic_indicator_rectangle")
                     .renderingMode(.template)
                     .foregroundColor(iconColor)
-                    .shadow(color: .main, radius: 4)
-                    .shadow(color: .main.opacity(0.5), radius: 8)
+                    .shadow(color: hasGlow ? .main : .clear, radius: hasGlow ? 4 : 0)
+                    .shadow(color: hasGlow ? .main.opacity(0.5) : .clear, radius: hasGlow ? 8 : 0)
+                    .frame(height: 0, alignment: .bottom)
+                    .offset(y: 2)
             }
             
             Image("ic_indicator_star")
                 .renderingMode(.template)
                 .foregroundColor(iconColor)
-                .shadow(
-                    color: isOngoing ? .main : .clear,
-                    radius: isOngoing ? 4 : 0
-                )
+                .shadow(color: hasGlow ? .main : .clear, radius: hasGlow ? 4 : 0)
         }
     }
 }
@@ -39,10 +53,12 @@ struct DailyJourneyTimelineIndicatorView: View{
 
 #Preview {
     ZStack {
-        Color.black
+        Color.kBlack
+        
         VStack(spacing: 40) {
-            DailyJourneyTimelineIndicatorView(isOngoing: true)
-            DailyJourneyTimelineIndicatorView(isOngoing: false)
+            DailyJourneyTimelineIndicatorView(isOngoing: true, status: "PENDING")
+            DailyJourneyTimelineIndicatorView(isOngoing: false, status: "PENDING")
+            DailyJourneyTimelineIndicatorView(isOngoing: false, status: "COMPLETE")
         }
     }
 }
