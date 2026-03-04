ff027d6e0cdf44be753438e7eb0ff5fb753dbbe4
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-03-04 19:13:12 +0900

    [feat] bottomSheet 연결 코드 추가

---
+++ Kiero/Kiero/Presentation/Common/Extensions/View+.swift
@@ -0,0 +1,31 @@
+//
+//  View+.swift
+//  Kiero
+//
+//  Created by 정윤아 on 3/4/26.
+//
+
+import SwiftUI
+
+extension View {
+    func showRewardBottomSheet(
+        reward: Reward,
+        onEdit: @escaping () -> Void,
+        onDelete: @escaping () -> Void
+    ) {
+        let detailData = DetailModel(
+            title: reward.title,
+            type: .reward(price: reward.cost)
+        )
+
+        let bottomSheet = DetailBottomSheet(data: detailData)
+        bottomSheet.onEditTap = onEdit
+        bottomSheet.onDeleteTap = onDelete
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootVC = windowScene.windows.first?.rootViewController {
+            let topVC = rootVC.presentedViewController ?? rootVC
+            topVC.present(bottomSheet, animated: false) // 내부 애니메이션 사용을 위해 false
+        }
+    }
+}
