f575c71fa64677576c26b304cba0a8a57966bbee
KooWihC <ahnchiwook@hanmail.net>
2026-03-09 03:58:34 +0900

    [feat] 이름과 꾸비 박스 구현

---
+++ Kiero/Kiero/Presentation/TodayStatus/View/ProfileCard.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileCard.swift
+//  Kiero
+//
+//  Created by 안치욱 on 3/7/26.
+//
+
+import SwiftUI
+
+struct ProfileCard: View {
+    let name = UserDefaults.standard.string(forKey: "firstName")
+
+    var body: some View {
+        HStack (alignment: .top){
+            VStack (alignment: .leading, spacing: 0){
+                HStack (alignment: .bottom, spacing: 0){
+                    Text("근영")
+                        .font(Font(UIFont.head2_20_B))
+                        .foregroundColor(.white)
+                    Text("의 오늘의 현황")
+                        .font(Font(UIFont.head3_16_B))
+                        .foregroundStyle(.white)
+                }
+                .frame(height: 24)
+                .padding(.leading, 16)
+                .padding(.top, 5)
+                
+                Text(Date().toFullDateString)
+                    .font(Font(UIFont.body4_12_R))
+                    .foregroundStyle(.gray500)
+                    .padding(.leading, 18)
+                    .padding(.top, 5.5)
+            }
+            
+            Spacer()
+            
+            Image(.imgGoblinSmile)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 84, height: 96)
+                .padding(.trailing, 40)
+        }
+    }
+}
