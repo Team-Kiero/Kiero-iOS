6e186e8d6d733d16719d7b27d3f2d45687b191f2
KooWihC <ahnchiwook@hanmail.net>
2026-03-09 14:59:04 +0900

    [feat] Mock데이터 주입, TodayStatusViewModel 구현

--- Kiero/Kiero/Presentation/TodayStatus/TodayStatusViewModel.swift
+++ Kiero/Kiero/Presentation/TodayStatus/TodayStatusViewModel.swift
@@ -8,6 +8,69 @@
 import UIKit
 import Combine
 
-final class TodayStatusViewModel: BaseViewModel {
+final class TodayStatusViewModel: BaseViewModel, ObservableObject {
+
+    @Published var completeMissions: [MissionDTO] = []
+    @Published var incompleteMissions: [MissionDTO] = []
+    @Published var schedules: [TodayScheduleDTO] = []
+    @Published var isFireLitToday: Bool = false
+
+    init(useMock: Bool = true) {
+        super.init()
+        if useMock {
+            loadMockData()
+        }
+    }
     
+    private func loadMockData() {
+        completeMissions = [
+            MissionDTO(name: "수학 숙제하기", reward: 50),
+            MissionDTO(name: "영어 숙제하기", reward: 50),
+            MissionDTO(name: "수학 숙제하기", reward: 50),
+            MissionDTO(name: "영어 숙제하기", reward: 50),
+            MissionDTO(name: "수학 숙제하기", reward: 50),
+            MissionDTO(name: "영어 숙제하기", reward: 50),
+            MissionDTO(name: "수학 숙제하기", reward: 50),
+            MissionDTO(name: "영어 숙제하기", reward: 50)
+        ]
+        
+        incompleteMissions = [
+            MissionDTO(name: "수학 숙제하기", reward: 100),
+            MissionDTO(name: "영어 숙제하기", reward: 50)
+        ]
+        
+        schedules = [
+            TodayScheduleDTO(
+                name: "피아노 학원",
+                startTime: "16:00",
+                endTime: "18:00",
+                imageUrl: "https://lgtm-images.lgtmeow.com/2025/08/12/09/3fcb0b3c-5476-4e4f-8b83-811bdf8868ad.webp",
+                status: .complete,
+                isNowSchedule: false
+            ),
+            TodayScheduleDTO(
+                name: "운동 하기",
+                startTime: "19:00",
+                endTime: "20:00",
+                imageUrl: "https://lgtm-images.lgtmeow.com/2023/11/04/00/bdd7d6c6-6e9b-4192-841a-e7afea219675.webp",
+                status: .verified,
+                isNowSchedule: true
+            ),
+            TodayScheduleDTO(
+                name: "독서 시간",
+                startTime: "19:00",
+                endTime: "19:30",
+                imageUrl: nil,
+                status: .pending,
+                isNowSchedule: false
+            )
+        ]
+        
+        isFireLitToday = true
+    }
+    
+    func fetchTodayStatus() {
+        
+        // TODO: -  API 호출
+    }
 }
