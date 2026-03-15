2d7dc18d38468b15f0851bf630a7a3b90a3bb7cc
KooWihC <ahnchiwook@hanmail.net>
2026-03-15 19:58:59 +0900

    [refactor] 네비게이션 바 제거 및 로직 수정

--- Kiero/Kiero/Presentation/Schedule/Schedule/ScheduleViewController.swift
+++ Kiero/Kiero/Presentation/Schedule/Schedule/ScheduleViewController.swift
@@ -15,12 +15,6 @@ class ScheduleViewController: BaseViewController<ScheduleViewModel> {
     
     // MARK: - UI Components
     
-    private lazy var navigationBar = NavigationBar(type: .main(title: "일정")).then {
-        $0.rightButtonAction = { [weak self] in
-            self?.presentNotificationFeed()
-        }
-    }
-    
     private lazy var scheduleChildVC: TimeTableViewController = {
         guard let viewModel = self.viewModel else { fatalError("ViewModel is missing") }
         let vc = AppDIContainer.shared.makeTimeTableViewController(viewModel: viewModel)
