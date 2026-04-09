05782eb36601670e96c808ec1189dd8d24bd9d1e
hyunseo-han <ols3040015@naver.com>
2026-04-09 14:28:41 +0900

    [refactor] 백그라운드 복귀 시 SSE 재연결 로직 추가

--- Kiero/Kiero/Application/SceneDelegate.swift
+++ Kiero/Kiero/Application/SceneDelegate.swift
@@ -69,14 +69,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        SseStreamManager.shared.resume()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+        SseStreamManager.shared.pause()
     }
 
 
