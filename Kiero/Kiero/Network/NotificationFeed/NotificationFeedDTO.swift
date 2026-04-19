328f4f0f058b7b8911a91df84b7d50f6ece18877
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-04-20 00:21:59 +0900

    [refactor] dto 순서 명세서와 동일하게 수정

--- Kiero/Kiero/Network/NotificationFeed/NotificationFeedDTO.swift
+++ Kiero/Kiero/Network/NotificationFeed/NotificationFeedDTO.swift
@@ -33,8 +33,8 @@ struct FeedMetadataDTO: Decodable {
 }
 
 struct UnreadNotificationDTO: Decodable {
-    let unreadChildIds: [Int]
     let hasUnread: Bool
+    let unreadChildIds: [Int]
 }
 
 extension FeedResponseDTO {
