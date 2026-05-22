fd94ccded64ef88995f61d9211473e2f6b6c1ef9
KooWihC <ahnchiwook@hanmail.net>
2026-05-22 21:59:19 +0900

    [refactor] SceneDelegate 수정

--- Kiero/Kiero/Application/SceneDelegate.swift
+++ Kiero/Kiero/Application/SceneDelegate.swift
@@ -10,28 +10,25 @@ import UIKit
 import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
     private var authCoordinator: AuthCoordinator?
-
+    
     func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-        
-        let window = UIWindow(windowScene: windowScene)
-        self.window = window
-        
-        let coordinator = AuthCoordinator(
-            window: window,
-            factory: AppDIContainer.shared
-        )
-        
-        self.authCoordinator = coordinator
-        coordinator.start()
-    }
+            _ scene: UIScene,
+            willConnectTo session: UISceneSession,
+            options connectionOptions: UIScene.ConnectionOptions
+        ) {
+            guard let windowScene = scene as? UIWindowScene else { return }
+            let window = UIWindow(windowScene: windowScene)
+            self.window = window
+            let coordinator = AuthCoordinator(
+                window: window,
+                factory: AppDIContainer.shared
+            )
+            self.authCoordinator = coordinator
+            coordinator.start()
+        }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         if let url = URLContexts.first?.url {
