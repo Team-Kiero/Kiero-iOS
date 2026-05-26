5472b8c49462c80eecc2d3420af2634552c9961f
KooWihC <ahnchiwook@hanmail.net>
2026-05-26 13:55:26 +0900

    [feat] 부분적 cornerRadius 조정 함수 추가

--- Kiero/Kiero/Presentation/Common/Extensions/View+.swift
+++ Kiero/Kiero/Presentation/Common/Extensions/View+.swift
@@ -31,4 +31,21 @@ extension View {
             topVC.present(bottomSheet, animated: false)
         }
     }
+    
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+private struct RoundedCorner: Shape {
+    let radius: CGFloat
+    let corners: UIRectCorner
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
 }
