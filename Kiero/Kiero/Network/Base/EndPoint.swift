d765eb74c47972556f8cc59b7c0a0a5bd4d8e3c9
hyunseo-han <ols3040015@naver.com>
2026-05-27 14:10:30 +0900

    [feat] 이용 약관 외부 링크 조회 API 연결 및 오픈소스 라이선스 행 제거

--- Kiero/Kiero/Network/Base/EndPoint.swift
+++ Kiero/Kiero/Network/Base/EndPoint.swift
@@ -44,7 +44,7 @@ enum EndPoint {
     case fetchDefaultColor(childId: Int)
     case editSchedule(scheduleId: Int, selectedDate: String, startDate: String?, endDate: String?)
     case deleteSchedule(scheduleId: Int, selectedDate: String?, startDate: String?, endDate: String?)
-
+    
     // Mission
     case fetchMissions(childId: Int?)
     case updateMission(missionId: Int, request: WriteMissionRequestDTO)
@@ -75,7 +75,7 @@ enum EndPoint {
         switch self {
         case .kakaoLogin, .kakaoAccessToken, .reissueAccessToken, .reissueAllTokens:
             return .none
-        case .fetchSchedules, .fetchChildrenInfo, .fetchWishes, .purchaseCoupon, .completeMission, .fireLit, .fetchJourneyList, .childSignup:
+        case .fetchSchedules, .fetchChildrenInfo, .fetchWishes, .purchaseCoupon, .completeMission, .fireLit, .fetchJourneyList, .childSignup, .fetchChildTerms:
             return .child
         default:
             return .parent
@@ -92,6 +92,9 @@ enum EndPoint {
     
     // MissionComplete
     case completeSchedule(scheduleDetailId: Int)
+    
+    // Terms
+    case fetchChildTerms
     
     // GiveFireStone
     case fireLit
@@ -154,6 +157,8 @@ enum EndPoint {
             return "/api/v1/presigned-url/schedules"
         case .completeSchedule(let scheduleDetailId):
             return "/api/v1/schedules/verify/\(scheduleDetailId)"
+        case .fetchChildTerms:
+            return "/api/v1/terms/children"
         case .fireLit:
             return "/api/v1/schedules/fire-lit"
         case .fetchDefaultColor(let childId):
@@ -194,7 +199,7 @@ enum EndPoint {
     
     var method: String {
         switch self {
-        case .checkConnection, .subscribeConnection, .fetchChildren, .fetchSchedules, .fetchChildrenInfo, .fetchWishes, .fetchMissions, .fetchDefaultColor, .fetchJourneyList, .fetchCoupons, .fetchTodayStatus, .fetchUnreadFeed:
+        case .checkConnection, .subscribeConnection, .fetchChildren, .fetchSchedules, .fetchChildrenInfo, .fetchWishes, .fetchMissions, .fetchDefaultColor, .fetchJourneyList, .fetchCoupons, .fetchTodayStatus, .fetchUnreadFeed, .fetchChildTerms:
             return "GET"
         case .updateDailyJourney, .skipJourney, .completeSchedule, .fireLit, .completeMission, .editSchedule, .updateMission, .updateCoupon, .postImageRead, .fetchFeeds:
             return "PATCH"
