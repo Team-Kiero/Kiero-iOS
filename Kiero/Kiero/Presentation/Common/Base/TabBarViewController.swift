4fa1fab7877effd12c0b5e5ec0217843bb8fa258
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-06-09 17:22:47 +0900

    [refactor] sse 중복 호출 로직 수정

--- Kiero/Kiero/Presentation/Common/Base/TabBarViewController.swift
+++ Kiero/Kiero/Presentation/Common/Base/TabBarViewController.swift
@@ -277,10 +277,10 @@ private extension TabBarViewController {
         
         NotificationCenter.default.addObserver(
             self,
-                selector: #selector(handleNavigateToWishWell),
-                name: .navigateToWishWell,
-                object: nil
-            )
+            selector: #selector(handleNavigateToWishWell),
+            name: .navigateToWishWell,
+            object: nil
+        )
         
         NotificationCenter.default.addObserver(
             self,
@@ -424,8 +424,8 @@ private extension TabBarViewController {
         print("🔄 포그라운드 복귀 - 동기화 시작")
         fetchInitialUnreadStatus()
         
-        globalSseStarted = false
-        startGlobalFeedSSEIfNeeded()
+        SseStreamManager.shared.pause()
+        SseStreamManager.shared.resume()
     }
     
     private func handleParentDeepLink(type: String) {
@@ -461,7 +461,7 @@ private extension TabBarViewController {
             if let nav = viewControllers?.first as? UINavigationController {
                 nav.popToRootViewController(animated: false)
             }
-
+            
         case "CHILD_MISSION_INCOMPLETE":
             selectedIndex = 1
             if let nav = viewControllers?[1] as? UINavigationController {
@@ -628,7 +628,7 @@ private extension TabBarViewController {
                     LogoutHelper.logoutToPickRole()
                     return
                 }
-
+                
                 guard payload.eventType == "FEED_ITEM_CREATED" else { return }
                 
                 NotificationCenter.default.post(
