1c7d35efb46c67a177baf7f8ceffccbf1d4120e1
hyunseo-han <ols3040015@naver.com>
2026-07-29 17:54:46 +0900

    [feat] 앰플리튜드 이벤트 및 프로퍼티 정의
    기획 문서 기준 P0 이벤트 18개와 User Properties를 타입으로 고정한다.
    호출부에서 문자열을 직접 쓰면 오타를 컴파일 타임에 잡을 수 없어 enum으로 정의.
    
    Event Properties는 문서에 개수(30~40개)만 있고 목록이 없어,
    현재 확정된 role과 invite_code_hash만 정의했다.

---
+++ Kiero/Kiero/Network/Analytics/AnalyticsEvent.swift
@@ -0,0 +1,57 @@
+//
+//  AnalyticsEvent.swift
+//  Kiero
+//
+
+import Foundation
+
+enum AnalyticsEvent: String {
+
+    // MARK: - 공통 / 온보딩
+
+    case appOpened = "app_opened"
+    case roleSelected = "role_selected"
+    case loginCompleted = "login_completed"
+    case termsAgreementCompleted = "terms_agreement_completed"
+    case inviteCodeCreated = "invite_code_created"
+    case childConnectionCompleted = "child_connection_completed"
+    case onboardingCompleted = "onboarding_completed"
+
+    // MARK: - 부모 핵심 행동
+
+    case scheduleCreated = "schedule_created"
+    case missionCreated = "mission_created"
+    case rewardCreated = "reward_created"
+
+    // MARK: - 자녀 핵심 행동
+
+    case scheduleAuthStarted = "schedule_auth_started"
+    case scheduleAuthCompleted = "schedule_auth_completed"
+    case scheduleSkipped = "schedule_skipped"
+    case dailyJourneyCompleted = "daily_journey_completed"
+    case missionCompleted = "mission_completed"
+    case wishPurchased = "wish_purchased"
+
+    // MARK: - 푸시 / 알림
+
+    case pushPermissionResult = "push_permission_result"
+    case pushClicked = "push_clicked"
+}
+
+enum AnalyticsEventProperty {
+    static let role = "role"
+    static let inviteCodeHash = "invite_code_hash"
+}
+
+enum AnalyticsUserProperty: String {
+    case role = "role"
+    case loginMethod = "login_method"
+    case childConnected = "child_connected"
+    case pushEnabled = "push_enabled"
+}
+
+enum AnalyticsLoginMethod: String {
+    case kakao
+    case apple
+    case inviteCode = "invite_code"
+}
