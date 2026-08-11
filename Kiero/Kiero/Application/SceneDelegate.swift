9a8559df21940ef49eba650057a598c9c32e688c
KooWihC <ahnchiwook@hanmail.net>
2026-08-12 03:04:46 +0900

    [feat] SceneDelegate에 Update Alert 함수 추가

--- Kiero/Kiero/Application/SceneDelegate.swift
+++ Kiero/Kiero/Application/SceneDelegate.swift
@@ -12,6 +12,7 @@ import KakaoSDKAuth
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private var isUpdateAlertPresented = false
     
 #if KIERO_PARENT
     var parentCoordinator: ParentCoordinator?
@@ -44,7 +45,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             }
         }
     }
-
     
     func changeRootViewController(_ vc: UIViewController, animated: Bool = true) {
         guard let window = self.window else { return }
@@ -67,8 +67,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        checkAppUpdate()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
@@ -82,5 +81,90 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         SseStreamManager.shared.pause()
+    }
+}
+
+private extension SceneDelegate {
+    
+    func checkAppUpdate() {
+        Task { @MainActor in
+            let updateType = await AppUpdateManager.shared.checkForUpdate()
+            handleUpdate(updateType)
+        }
+    }
+    
+    func handleUpdate(_ updateType: AppUpdateType) {
+        switch updateType {
+        case .none:
+            break
+            
+        case .optional:
+            showOptionalUpdateAlert()
+            
+        case .required:
+            showRequiredUpdateAlert()
+        }
+    }
+    
+    func showOptionalUpdateAlert() {
+        guard !isUpdateAlertPresented else { return }
+        guard let viewController = topViewController() else { return }
+        
+        isUpdateAlertPresented = true
+        
+        let alert = UIAlertController(title: "새로운 업데이트",
+                                      message: "새로운 버전이 출시되었어요.\n업데이트하시겠어요?",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "나중에", style: .cancel) { [weak self] _ in
+            self?.isUpdateAlertPresented = false
+        })
+        
+        alert.addAction(UIAlertAction(title: "업데이트", style: .default) { [weak self] _ in
+            self?.isUpdateAlertPresented = false
+            self?.openAppStore()
+        })
+        
+        viewController.present(alert, animated: true)
+    }
+    
+    func showRequiredUpdateAlert() {
+        guard !isUpdateAlertPresented else { return }
+        guard let viewController = topViewController() else { return }
+        
+        isUpdateAlertPresented = true
+        
+        let alert = UIAlertController(title: "필수 업데이트",
+                                      message: "서비스 이용을 위해\n최신 버전으로 업데이트해 주세요.",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "업데이트", style: .default) { [weak self] _ in
+            self?.openAppStore()
+        })
+        
+        viewController.present(alert, animated: true)
+    }
+    
+    func openAppStore() {
+        print(Config.appStoreURL)
+        UIApplication.shared.open(Config.appStoreURL)
+    }
+    
+    func topViewController(from viewController: UIViewController? = nil) -> UIViewController? {
+        let current = viewController ?? window?.rootViewController
+        
+        if let navigationController = current as? UINavigationController {
+            return topViewController(from: navigationController.visibleViewController)
+        }
+        
+        if let tabBarController = current as? UITabBarController {
+            return topViewController(from: tabBarController.selectedViewController)
+        }
+        
+        if let presentedViewController = current?.presentedViewController {
+            return topViewController(from: presentedViewController)
+        }
+        
+        return current
     }
 }
